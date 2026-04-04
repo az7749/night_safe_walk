8929e2b06ec4b1eace9dad3e40bb26511ed59697
az7749 <az7749@naver.com>
2026-04-04 19:08:37 +0900

    로그인UI 수정, 기본 구조 추가

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -4,7 +4,7 @@ class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LogineScreen();
+  State<LoginScreen> createState() => _LoginScreen();
 }
 
 class _LoginScreen extends State<LoginScreen> {
@@ -24,5 +24,64 @@ class _LoginScreen extends State<LoginScreen> {
 
     debugPrint('아이디 : $userid');
     debugPrint('비밀번호 : $password');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              // 아이디 입력창
+              TextField(
+                controller: useridController,
+                decoration: InputDecoration(
+                  hintText: '아이디',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              // 비밀번호 입력창
+              TextField(
+                controller: passwordController,
+
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.visibility),
+                  hintText: '비밀번호',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              // 로그인 버튼
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: handleLogin,
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: Text('로그인'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
